67ab844bfe638803ac90645f441317a4ae538e3e
dlancer <dmdpost@gmail.com>
2014-06-04 16:04:35 +0600

    form submitting signals: missed site argument added,  ip_sender argument renamed to ip

--- contact_form/signals.py
+++ contact_form/signals.py
@@ -1,9 +1,9 @@
 import django.dispatch
 
 contact_form_valid = django.dispatch.Signal(
-    providing_args=['event', 'sender_ip', 'sender_name', 'sender_email', 'email', 'subject', 'message']
+    providing_args=['event', 'ip', 'site', 'sender_name', 'sender_email', 'email', 'subject', 'message']
 )
 
 contact_form_invalid = django.dispatch.Signal(
-    providing_args=['event', 'sender_ip', 'sender_name', 'sender_email']
+    providing_args=['event', 'ip', 'site', 'sender_name', 'sender_email']
 )
