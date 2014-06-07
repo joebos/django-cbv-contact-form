8eb49de0871082dd888cd699b439371eb16205bc
dlancer <dmdpost@gmail.com>
2014-06-07 14:11:59 +0600

    remove pip requirements from setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,10 @@
 import os
 from setuptools import setup, find_packages
 from contact_form import get_version as get_package_version
-from pip.req import parse_requirements
 
 README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 REQUIREMENTS = os.path.join(os.path.dirname(__file__), 'requirements.txt')
-install_reqs = parse_requirements(REQUIREMENTS)
-reqs = [str(ir.req) for ir in install_reqs]
+reqs = open(REQUIREMENTS).read().splitlines()
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
